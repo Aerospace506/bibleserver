6940dc7cbd234a6dd9523bea42bcc0635fd14cdf
st221515 <g.atulegwu@stud.hs-wismar.de>
2025-02-24 11:41:36 +0100

    update hypercorn file

--- hypercorn_config.py
+++ hypercorn_config.py
@@ -1,9 +1,2 @@
-
-
-# Production server configuration
-
-bind = "0.0.0.0:${PORT}"
-workers = 2
-keep_alive_timeout = 300
-timeout = 120
-accesslog = "-"  # Log to stdout
+bind = "0.0.0.0:8008"  # Hardcode port temporarily
+workers = 1
